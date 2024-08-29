f2159f549cd99a8a1b3d560f6dd0b08f90e8bb66
DaryPet <d7akkord@gmail.com>
2024-08-29 16:02:59 +0200

    camperCard- jsx+css

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -1,35 +1,86 @@
 import { Link } from "react-router-dom";
 import css from "./CamperCard.module.css";
-import { FaRegHeart } from "react-icons/fa";
+import {
+  FaRegHeart,
+  FaStar,
+  FaMapMarkedAlt,
+  FaFan,
+  FaGasPump,
+  FaCarSide,
+  FaCoffee,
+} from "react-icons/fa";
 
 export default function CamperCard({ camper }) {
+  const imageUrl =
+    camper.gallery && camper.gallery.length > 0
+      ? camper.gallery[0].thumb
+      : "path/to/placeholder-image.jpg";
+
   return (
     <div className={css.card}>
-      <img src={camper.image} alt={camper.name} className={css.image} />
-      <div className={css.details}>
-        <div className={css.header}>
-          <h3 className={css.name}>{camper.name}</h3>
-          <span className={css.reviews}>{camper.reviews.length} Reviews</span>
-        </div>
-        <p className={css.description}>{camper.description}</p>
-
-        <div className={css.features}>
-          {camper.ac && <span className={css.feature}>AC</span>}
-          {camper.automatic && <span className={css.feature}>Automatic</span>}
-          {camper.petrol && <span className={css.feature}>Petrol</span>}
-          {camper.kitchen && <span className={css.feature}>Kitchen</span>}
-        </div>
-
-        <p className={css.price}>
-          €{camper.price.toFixed(2).replace(".", ",")}
-        </p>
-        <Link to={`/catalog/${camper.id}`} className={css.moreButton}>
-          Show more
-        </Link>
+      <div>
+        <img src={imageUrl} alt={camper.name} className={css.image} />
       </div>
-      <button className={css.favoriteButton}>
-        <FaRegHeart />
-      </button>
+
+      <div className={css.info}>
+        <div className={css.details}>
+          <div className={css.header}>
+            <h3 className={css.name}>{camper.name}</h3>
+            <p className={css.price}>
+              €{camper.price.toFixed(2).replace(".", ",")}
+              <button className={css.favoriteButton}>
+                <FaRegHeart className={css.likeIcon} />
+              </button>
+            </p>
+          </div>
+
+          <div className={css.subheader}>
+            <span className={css.raiting}>
+              <FaStar className={css.starIcon} />
+              {camper.rating}
+            </span>
+            <span className={css.reviewCount}>
+              ({camper.reviews.length} Reviews)
+            </span>
+            <span className={css.location}>
+              <FaMapMarkedAlt className={css.locationIcon} /> {camper.location}
+            </span>
+          </div>
+          <div>
+            <p className={css.description}>{camper.description}</p>
+
+            <div className={css.features}>
+              {camper.transmission && (
+                <span className={css.feature}>
+                  <FaCarSide className={css.icon} />
+                  Automatic
+                </span>
+              )}
+              {camper.gas && (
+                <span className={css.feature}>
+                  <FaGasPump className={css.icon} />
+                  Gas
+                </span>
+              )}
+              {camper.kitchen && (
+                <span className={css.feature}>
+                  <FaCoffee className={css.icon} />
+                  Kitchen
+                </span>
+              )}
+              {camper.AC && (
+                <span className={css.feature}>
+                  <FaFan className={css.icon} />
+                  AC
+                </span>
+              )}
+            </div>
+            <Link to={`/catalog/${camper.id}`} className={css.moreButton}>
+              Show more
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
